b633adbea320c8d77270626377d3cdae17924734
Charm Sum Yu <samsoncsyu77771402@gmail.com>
2021-11-16 18:04:51 -0500

    1st commit
    -Set up Amazon S3
    -A new route for uploading an image to Amazon and get a URL of the uploaded image
    -Save the URL in Profile model

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,3 +1,5 @@
+const upload = require("../services/ImageUpload");
+const singleUpload = upload.single("image");
 const Profile = require("../models/Profile");
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
@@ -37,4 +39,31 @@ exports.loadProfile = asyncHandler(async (req, res, next) => {
       profile: profile,
     },
   });
+});
+
+// @route POST /profile/upload-image
+// @desc upload user's image
+// @access Private
+exports.uploadProfileImage = asyncHandler(async (req, res, next) => {
+  const id = req.user.id;
+
+  singleUpload(req, res, function (err) {
+    if (err) {
+      return res.json({
+        success: false,
+        errors: {
+          title: "Image Upload Error",
+          detail: err.message,
+          error: err,
+        },
+      });
+    }
+
+    let update = { photo: req.file.location };
+
+    Profile.findOneAndUpdate({ userId: id }, update, { new: true })
+      .then((user) => res.status(200).json({ success: true, user: user }))
+      .catch((err) => res.status(400).json({ success: false, error: err }));
+  });
+
 });
