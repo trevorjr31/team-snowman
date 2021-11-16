324b23bff8f9b11960bd51dc97eb85b4ccf06ebb
Andrew Hodge <hodge.iaj@gmail.com>
2021-11-16 11:07:07 -0800

    profile model hotfix

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -5,35 +5,29 @@ const timeSlot = new mongoose.Schema({ start: Date, end: Date });
 const profileSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    default: "",
   },
   lastName: {
     type: String,
-    default: "",
   },
   description: {
     type: String,
-    default: "",
   },
   address: {
     type: String,
-    default: "",
   },
   phoneNumber: {
     type: String,
-    default: "",
   },
   dateOfBirth: {
     type: Date,
     default: null
   },
   availability: {
-    type: [Date],
+    type: [timeSlot],
     default: []
   },
   photo: {
     type: String,
-    default: "",
   },
 });
 
