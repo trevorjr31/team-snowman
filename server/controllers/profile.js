4815c22bf611193da3959a93648b461fa19d30fa
Charm Sum Yu <samsoncsyu77771402@gmail.com>
2021-11-16 22:56:13 -0500

    update promise and callback issue

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -59,11 +59,20 @@ exports.uploadProfileImage = asyncHandler(async (req, res, next) => {
       });
     }
 
-    let update = { photo: req.file.location };
+    const update = { photo: req.file.location };
 
-    Profile.findOneAndUpdate({ userId: id }, update, { new: true })
-      .then((user) => res.status(200).json({ success: true, user: user }))
-      .catch((err) => res.status(400).json({ success: false, error: err }));
+    try {
+      Profile.findOneAndUpdate({ userId: id }, update, {
+        new: true,
+      });
+      res
+        .status(200)
+        .send({ message: "The profile has been successfully updated" });
+    } catch (err) {
+      res.status(500);
+      throw new Error("Server failed to update the profile");
+    }
+
   });
 
 });
