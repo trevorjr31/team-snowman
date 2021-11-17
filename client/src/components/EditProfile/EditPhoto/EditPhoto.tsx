b9cc6b4311feee5e0064db1c1ab249aa5eec1930
Samson Samson <samsoncsyuapple@gmail.com>
2021-11-17 15:26:39 -0500

    1st commit

---
+++ client/src/components/EditProfile/EditPhoto/EditPhoto.tsx
@@ -0,0 +1,58 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import useStyles from './useStyles';
+import { useAuth } from '../../../context/useAuthContext';
+import { useSnackBar } from '../../../context/useSnackbarContext';
+import logo from '../../../Images/775db5e79c5294846949f1f55059b53317f51e30.png';
+
+export default function EditPhoto(): JSX.Element {
+  const classes = useStyles();
+  const { updateLoginContext } = useAuth();
+  const { updateSnackBarMessage } = useSnackBar();
+
+  /*const handleSubmit = (
+    { email, password }: { email: string; password: string },
+    { setSubmitting }: FormikHelpers<{ email: string; password: string }>,
+  ) => {
+    login(email, password).then((data) => {
+      if (data.error) {
+        setSubmitting(false);
+        updateSnackBarMessage(data.error.message);
+      } else if (data.success) {
+        updateLoginContext(data.success);
+      } else {
+        // should not get here from backend but this catch is for an unknown issue
+        console.error({ data });
+
+        setSubmitting(false);
+        updateSnackBarMessage('An unexpected error occurred. Please try again');
+      }
+    });
+  };*/
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={7} elevation={4} component={Paper} square>
+        <Box className={classes.authWrapper}>
+          <Box width="100%" maxWidth={450} p={3} alignSelf="center">
+            <Grid container>
+              <Grid item xs>
+                <Typography className={classes.welcome} component="h1" variant="h5">
+                  Profile Photo
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid className={classes.centerRow}>
+              <Avatar src={logo} className={classes.avatar} />
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
