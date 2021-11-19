c68c16aede6ceb274cb1f388e73ee28f592e79af
trevorjr31 <77120652+trevorjr31@users.noreply.github.com>
2021-11-19 07:05:39 -0500

    updated calendar

--- client/src/components/Requests/RequestCaldendar/RequestCalendar.tsx
+++ client/src/components/Requests/RequestCaldendar/RequestCalendar.tsx
@@ -1,18 +1,12 @@
 import Box from '@material-ui/core/Box';
 import { useRequest } from '../../../context/useRequestContext';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import useStyles from './useStyles';
 import { Calendar } from 'react-multi-date-picker';
 import 'react-multi-date-picker/styles/colors/red.css';
-import { useState } from 'react';
 export default function RequestCalendar(): JSX.Element {
   const classes = useStyles();
   const { requests } = useRequest();
-  const today = new Date();
-  const tomorrow = new Date();
-
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  const [values, setValues] = useState([today, tomorrow]);
   if (requests) {
     const allRequests = [...requests.before, ...requests.after, requests.nextRequest];
     const entries = [new Date()];
