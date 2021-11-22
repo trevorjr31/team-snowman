923025904522db56a8f1966e3e1b006b4f7bedc3
trevorjr31 <77120652+trevorjr31@users.noreply.github.com>
2021-11-22 16:18:13 -0500

    Revert "Revert "Merge branch 'request-ui' into requests""
    This reverts commit 228f62d959628775076bd2a53f3ff2a98f33d71e.

---
+++ client/src/components/Requests/RequestCaldendar/RequestCalendar.tsx
@@ -0,0 +1,32 @@
+import Box from '@material-ui/core/Box';
+import { useRequest } from '../../../context/useRequestContext';
+import { Grid } from '@material-ui/core';
+import useStyles from './useStyles';
+import { Calendar } from 'react-multi-date-picker';
+import 'react-multi-date-picker/styles/colors/red.css';
+import RequestData from '../../../interface/Request';
+
+interface Props {
+  requests: RequestData;
+}
+
+export default function RequestCalendar({ requests }: Props): JSX.Element {
+  const classes = useStyles();
+  const allRequests = requests.nextRequest
+    ? [...requests.before, ...requests.after, requests.nextRequest]
+    : [...requests.before, ...requests.after];
+  const entries = [new Date()];
+  allRequests.map((request) => {
+    if (request.accepted == true) {
+      const date = new Date(request.duration.start);
+      entries.push(date);
+    }
+  });
+  return (
+    <Grid className={classes.root}>
+      <Box marginTop={33} className={classes.calendar} marginLeft={22}>
+        <Calendar readOnly showOtherDays value={entries} disableMonthPicker hideWeekDays className="red" />
+      </Box>
+    </Grid>
+  );
+}
