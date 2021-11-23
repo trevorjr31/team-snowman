552ef30e9c87a087d62058da032df249fd81df29
Andrew Hodge <hodge.iaj@gmail.com>
2021-11-23 12:02:52 -0800

    added style classes to datepicker

--- client/src/components/DatePicker/DatePicker.tsx
+++ client/src/components/DatePicker/DatePicker.tsx
@@ -3,6 +3,7 @@ import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import useStyles from './useStyles';
 
 export default function MaterialUIPickers() {
   const [value, setValue] = React.useState<Date | null>(new Date('2014-08-18T21:11:54'));
@@ -11,14 +12,14 @@ export default function MaterialUIPickers() {
     setValue(newValue);
   };
 
+  const classes = useStyles();
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DesktopDatePicker
-        label="Birthday"
         inputFormat="MM/dd/yyyy"
         value={value}
         onChange={handleChange}
-        renderInput={(params) => <TextField {...params} margin="normal" />}
+        renderInput={(params) => <TextField {...params} className={classes.birthday} />}
       />
     </LocalizationProvider>
   );
