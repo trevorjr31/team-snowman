d9a629b9f9209f14c5716d6b7cb30cd1ea5ab0b2
Charm Sum Yu <samsoncsyu77771402@gmail.com>
2021-11-30 14:17:39 -0500

    Update mockUser.ts

--- client/src/mocks/mockUser.ts
+++ client/src/mocks/mockUser.ts
@@ -1,25 +1,29 @@
 import { User } from '../interface/User';
-
+import { mockLoggedInUserProfile, mockOtherUsersProfile } from './mockUserProfile';
 const mockLoggedInUser: User = {
   email: 'mockLoggedInUser@gmail.com',
   username: 'mock LoggedIn user',
   id: '1',
+  profile: mockLoggedInUserProfile,
 };
 
 const mockOtherUser1: User = {
   username: 'Mock test user 1',
   email: 'mockTestUser1@gmail.com',
   id: '2',
+  profile: mockOtherUsersProfile[0],
 };
 const mockOtherUser2: User = {
   username: 'Mock test user 2',
   email: 'mockTestUser2@gmail.com',
   id: '3',
+  profile: mockOtherUsersProfile[1],
 };
 const mockOtherUser3: User = {
   username: 'Mock test user 3',
   email: 'mockTestUser3@gmail.com',
   id: '4',
+  profile: mockOtherUsersProfile[2],
 };
 
 const mockOtherUsers: User[] = [mockOtherUser1, mockOtherUser2, mockOtherUser3];
